60c3c649e89e997eda04cf9c0fa3a9adf10f7666
steffeno <steffeno-etc@protonmail.com>
2023-05-09 20:25:43 +0200

    fixed exercise 1.39

--- 1/39/main.scm
+++ 1/39/main.scm
@@ -10,8 +10,9 @@
 (define (square x) (* x x))
 (define (nth-odd n) (- (* n 2) 1))
 (define (tan-cf x k)
-  (define (n i) (* (- 0 1) (square x)))
+  (define (n i)
+    (if (= i 1)
+        x
+        (- (square x))))
   (define (d i) (nth-odd i))
   (- 0 (cont-frac n d k)))
-
-(display (tan-cf 3.14 100))
