d1d9041cdb0207bb1d86cb666175b0fdaf62deb1
steffeno <steffeno-etc@protonmail.com>
2023-05-10 19:02:52 +0200

    added ex. 2.2

---
+++ 2/2/main.scm
@@ -0,0 +1,29 @@
+(define (make-point x y) (cons x y))
+(define (x-point p) (car p))
+(define (y-point p) (cdr p))
+
+(define (print-point p)
+  (newline)
+  (display "(")
+  (display (x-point p))
+  (display ",")
+  (display (y-point p))
+  (display ")"))
+
+(define (make-segment p q) (cons p q))
+(define (start-segment s) (car s))
+(define (end-segment s) (cdr s))
+
+(define (average a b) (/ (+ a b) 2))
+
+(define (midpoint-segment s)
+  (let ((start (start-segment s))
+        (end (end-segment s)))
+    (make-point (average (x-point start)
+                         (x-point end))
+                (average (y-point start)
+                         (y-point end)))))
+
+(print-point (midpoint-segment
+               (make-segment (make-point 2 2)
+                             (make-point 6 4))))
