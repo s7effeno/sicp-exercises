eb369e5c4e85d7b683f972b5383d15df5b7a51c1
steffeno <steffeno-etc@protonmail.com>
2023-05-10 16:34:19 +0200

    started chapter 2, added exercise 2.1

---
+++ 2/1/main.scm
@@ -0,0 +1,25 @@
+(define (gcd a b)
+  (if (= b 0)
+      (abs a)
+      (gcd b (remainder a b))))
+
+(define (make-rat n d)
+  (let ((g (gcd n d)))
+    (if (< d 0)
+        (make-rat (- n) (- d))
+        (cons (/ n g)
+              (/ d g)))))
+
+(define (numer x) (car x))
+(define (denom x) (cdr x))
+
+(define (print-rat x)
+  (display (numer x))
+  (display "/")
+  (display (denom x))
+  (newline))
+
+(print-rat (make-rat 1 2))
+(print-rat (make-rat (- 1) 2))
+(print-rat (make-rat 1 (- 2)))
+(print-rat (make-rat (- 1) (- 2)))
