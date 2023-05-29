c7f1d5d1d5adfc5ede10d0a71906836626f6b1bb
steffeno <steffeno-etc@protonmail.com>
2023-05-29 21:34:32 +0200

    added exercise 2.21

---
+++ 2/21/main.scm
@@ -0,0 +1,15 @@
+(define nil (list))
+(define (square x) (* x x))
+
+(define (square-list items)
+  (if (null? items)
+      nil
+      (cons (square (car items))
+            (square-list (cdr items)))))
+(display (square-list (list 1 2 3 4)))
+(newline)
+
+(define (square-list items)
+  (map square items))
+(display (square-list (list 1 2 3 4)))
+(newline)
