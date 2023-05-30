c876bf199f64ee90855c015f63e62c51af83413a
steffeno <steffeno-etc@protonmail.com>
2023-05-30 11:04:40 +0200

    fixed exercise 2.18

--- 2/18/main.scm
+++ 2/18/main.scm
@@ -1,8 +1,10 @@
-(define (reverse l)
-  (if (null? (cdr l))
-      (car l)
-      (cons (reverse (cdr l))
-            (car l))))
+(define nil (list))
+
+(define (reverse items)
+  (define (iter items ret)
+    (if (null? items)
+      ret
+      (iter (cdr items) (cons (car items) ret))))
+  (iter items nil))
 
 (display (reverse (list 1 4 9 16 25)))
-// FIXME
