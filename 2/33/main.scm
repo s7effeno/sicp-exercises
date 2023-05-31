b2338f0c8ffac809b012452af6bf89113f3eaad6
steffeno <steffeno-etc@protonmail.com>
2023-05-31 14:04:03 +0200

    added exercise 2.33

---
+++ 2/33/main.scm
@@ -0,0 +1,26 @@
+(define nil (list))
+
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op
+                      initial
+                      (cdr sequence)))))
+(define (map p sequence)
+  (accumulate (lambda (x y) (cons (p x) y))
+              nil sequence))
+
+(define (append seq1 seq2)
+  (accumulate cons seq2 seq1))
+
+(define (length sequence)
+  (accumulate (lambda (x y) (+ 1 y)) 0 sequence))
+
+(define (square x) (* x x))
+(define x (list 1 2 3))
+(display (map square x))
+(newline)
+(display (append x x))
+(newline)
+(display (length x))
