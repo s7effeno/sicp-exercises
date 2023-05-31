4ff710945d07aa5538ebd0254652218da661d29a
steffeno <steffeno-etc@protonmail.com>
2023-05-31 15:15:36 +0200

    added exercise 2.36

---
+++ 2/36/main.scm
@@ -0,0 +1,26 @@
+(define nil (list))
+
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op
+                      initial
+                      (cdr sequence)))))
+
+(define (accumulate-n op init seqs)
+  (if (null? (car seqs))
+      nil
+      (cons (accumulate op init (map (lambda (seq)
+                                       (car seq))
+                                     seqs))
+            (accumulate-n op init (map (lambda (seq)
+                                         (cdr seq))
+                                       seqs)))))
+
+(display (accumulate-n +
+                       0
+                       (list (list 1 2 3)
+                             (list 4 5 6)
+                             (list 7 8 9)
+                             (list 10 11 12))))
