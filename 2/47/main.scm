8a6dbc2a2e374a02d267e0c0cffe5d8fa69f2f73
steffeno <steffeno-etc@protonmail.com>
2023-09-16 23:07:48 +0200

    added exercise 2.47

---
+++ 2/47/main.scm
@@ -0,0 +1,23 @@
+(define (make-frame origin edge1 edge2)
+  (list origin edge1 edge2))
+
+(define (frame-orig frame)
+  (car frame))
+
+(define (frame-edge1 frame)
+  (cadr frame))
+
+(define (frame-edge2 frame)
+  (caddr frame))
+
+(define (make-frame origin edge1 edge2)
+  (cons origin (cons edge1 edge2)))
+
+(define (frame-orig frame)
+  (car frame))
+
+(define (frame-edge1 frame)
+  (cadr frame))
+
+(define (frame-edge2 frame)
+  (cddr frame))
