854c51fd3d60a0c1a5b5305d97a429b1e1f43f7c
steffeno <steffeno-etc@protonmail.com>
2023-09-25 02:48:00 +0200

    added exercise 2.51

---
+++ 2/51/main.scm
@@ -0,0 +1,42 @@
+(define (below painter1 painter2)
+  (let ((split-point (make-vect 0.0 0.5)))
+      (let ((paint-bottom (transform-painter painter1
+                                             (make-vect 0.0 0.0)
+                                             (make-vect 1.0 0.0)
+                                             split-point))
+            (paint-top (transform-painter painter2
+                                          split-point
+                                          (make-vect 1.0 0.5)
+                                          (make-vect 0.0 1.0)))))))
+        (lambda (frame)
+          (paint-bottom frame)
+          (paint-top frame)))))
+
+(define (beside painter1 painter2)
+  (let ((split-point (make-vect 0.5 0.0)))
+    (let ((paint-left  (transform-painter 
+                        painter1
+                        (make-vect 0.0 0.0)
+                        split-point
+                        (make-vect 0.0 1.0)))
+          (paint-right (transform-painter
+                        painter2
+                        split-point
+                        (make-vect 1.0 0.0)
+                        (make-vect 0.5 1.0))))
+      (lambda (frame)
+        (paint-left frame)
+        (paint-right frame)))))
+
+(define (rotate90 painter)
+  (transform-painter painter
+                     (make-vect 1.0 0.0)
+                     (make-vect 1.0 1.0)
+                     (make-vect 0.0 0.0)))
+
+(define (rotate270 painter)
+  (rotate90 (rotate180 painter)))
+
+(define (below painter1 painter2)
+  (rotate90 (beside (rotate270 painter1)
+                    (rotate270 painter2))))
