79105240d6d3a84f1ea650ab58cc16c8ba4996ce
steffeno <steffeno-etc@protonmail.com>
2023-09-25 14:35:40 +0200

    added exercise 2.52

---
+++ 2/52/main.scm
@@ -0,0 +1,32 @@
+(define wave->painter
+  ; TODO)
+
+(define (corner-split painter n)
+  (if (= n 0)
+      painter
+      (let ((up (up-split painter (- n 1)))
+            (right (right-split painter 
+                                (- n 1))))
+        (let ((top-left up)
+              (bottom-right right)
+              (corner (corner-split painter 
+                                    (- n 1))))
+          (beside (below painter top-left)
+                  (below bottom-right 
+                         corner))))))
+
+(define (square-of-four tl tr bl br)
+  (lambda (painter)
+    (let ((top (beside (tl painter) 
+                       (tr painter)))
+          (bottom (beside (bl painter) 
+                          (br painter))))
+      (below bottom top))))
+
+(define (square-limit painter n)
+  (let ((combine4 
+         (square-of-four identity 
+                         flip-oriz
+                         flip-vert 
+                         rotate180)))
+    (combine4 (corner-split painter n))))
