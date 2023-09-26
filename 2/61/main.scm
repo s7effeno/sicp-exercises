4cfdf4eac5536841922e6d4859bdf67cc5c3ceca
steffeno <steffeno-etc@protonmail.com>
2023-09-26 02:09:29 +0200

    added exercise 2.61

---
+++ 2/61/main.scm
@@ -0,0 +1,8 @@
+(define (adjoin-set x set)
+  (cond ((or (null? set)
+             (< x (car set)))
+         (cons x set))
+        ((= x (car set))
+         set)
+        (else (cons (car set)
+                    (adjoin-set x (cdr set))))))
