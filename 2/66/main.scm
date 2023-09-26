944d56142f9b12b76c5cc4ae0ceacd0f38fee0d3
steffeno <steffeno-etc@protonmail.com>
2023-09-26 18:24:09 +0200

    added exercise 2.66

---
+++ 2/66/main.scm
@@ -0,0 +1,12 @@
+(define (entry tree) (car tree))
+(define (left-branch tree) (cadr tree))
+(define (right-branch tree) (caddr tree))
+
+(define (lookup given-key set-of-records)
+  (cond ((null? set-of-records) #f)
+        ((< given-key (key (entry set-of-records)))
+         (lookup given-key (left-branch set-of-records)))
+        ((> given-key (key (entry set-of-records)))
+         (lookup given-key (right-branch set-of-records)))
+        ; (= given-key (key (entry set-of-records)))
+        (else (entry set-of-records))))
