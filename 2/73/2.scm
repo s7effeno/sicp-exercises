6cf0a0ed220e82cdfdc75471f9e21fa16126afc2
steffeno <steffeno-etc@protonmail.com>
2023-11-01 08:27:01 +0100

    completed exercise 2.73

--- 2/73/2.scm
+++ 2/73/2.scm
@@ -1,16 +1,16 @@
+(define (make-sum a1 a2) (list a1 '+ a2))
+(define (addend s) (car s))
+(define (augend s) (caddr s))
 (define (install-sum-package)
-  (define (make-sum a1 a2) (list a1 '+ a2))
-  (define (addend s) (car s))
-  (define (augend s) (caddr s))
   (define (deriv-sum exp var)
    (make-sum (deriv (addend exp) var)
              (deriv (augend exp) var)))
   (put 'deriv ('+) deriv-sum))
 
+(define (make-product m1 m2) (list m1 '* m2))
+(define (multiplier p) (car p))
+(define (multiplicand p) (caddr p))
 (define (install-product-package)
-  (define (make-product m1 m2) (list m1 '* m2))
-  (define (multiplier p) (car p))
-  (define (multiplicand p) (caddr p))
   (define (deriv-product exp var)
     (make-sum
       (make-product
--- 2/73/3.scm
+++ 2/73/3.scm
@@ -1,1 +1,17 @@
-; TODO
+(define (base e)
+  (cadr e))
+(define (exponent e)
+  (caddr e))
+(define (make-exponentiation b e)
+  (cond ((= e 0) 1)
+        ((= e 1) b)
+        (else (list '** b e))))
+(define (install-exponentiation-package)
+  (define (deriv-exponentiation exp var)
+    (make-product
+      (make-product
+        (exponent exp)
+        (make-exponentiation
+          (base exp)
+          (make-sum (exponent exp) '-1)))))
+  (put 'deriv '(**) deriv-exponentiation))
